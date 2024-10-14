a28c8d6bbba1bc91d46fe2d340df8e479b36c843
edy alves <30.edyalves@gmail.com>
2024-10-14 10:07:47 -0300

    Desenhando simbolo do emblema.

---
+++ Landmarks/Views/Badges/BadgeSymbol.swift
@@ -0,0 +1,18 @@
+//
+//  BadgeSymbol.swift
+//  Landmarks
+//
+//  Created by ednardo alves on 13/10/24.
+//
+
+import SwiftUI
+
+struct BadgeSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BadgeSymbol()
+}
---
+++ Landmarks/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,18 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Landmarks
+//
+//  Created by ednardo alves on 14/10/24.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RotatedBadgeSymbol()
+}
