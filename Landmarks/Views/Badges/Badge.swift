d8f0cc4ed9eaaa062087c13fc983bced0dece894
edy alves <30.edyalves@gmail.com>
2024-10-14 10:32:33 -0300

    Finalizacao do emblema

---
+++ Landmarks/Views/Badges/Badge.swift
@@ -0,0 +1,18 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by ednardo alves on 14/10/24.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Badge()
+}
