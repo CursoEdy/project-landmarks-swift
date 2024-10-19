ec129ad43cd4bd87af59c0e65da6fdce5e95e4dd
edy alves <30.edyalves@gmail.com>
2024-10-19 19:53:35 -0300

    Definindo o editor de perfil

---
+++ Landmarks/Views/Profiles/ProfileEditor.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileEditor.swift
+//  Landmarks
+//
+//  Created by ednardo alves on 19/10/24.
+//
+
+import SwiftUI
+
+struct ProfileEditor: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileEditor()
+}
